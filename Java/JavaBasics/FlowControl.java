5fbb216b920b8cff5c3f4ef91eaea481260330b3
letmesoloall <letmesoloall@gmail.com>
2024-01-02 19:54:37 +0530

    Day 2 JavaBasics

--- Java/JavaBasics/FlowControl.java
+++ Java/JavaBasics/FlowControl.java
@@ -2,6 +2,36 @@ package JavaBasics;
 
 public class FlowControl {
     public static void main(String[] args) {
+        // if block
+
+        if (true){
+
+            // statement here
+
+        }
+
+        // If else
+
+        if (false){
+            
+            // statment won't execute
+        }
+        else{
+            // This statement will run
+        }
+
+
+        // If elif block 
+
+        if(false){
+            // statment
+        }
+
+        else if (true){
+
+            // statement
+        }
+
         
     }
 }
---
+++ Java/JavaBasics/Loops.java
@@ -0,0 +1,35 @@
+package JavaBasics;
+public class Loops {
+    public static void main(String[] args) {
+        
+
+        // while loop 
+
+
+        //while(true){
+            //statement
+        //}
+
+
+        // for loop 
+        
+
+        for (int i = 0; i<=1; i++){
+            // statement 
+        }
+
+
+        // do while statements
+
+        do{
+            //statement
+        }
+        while(true);
+
+
+        // for each loop
+
+        
+
+    }
+}
