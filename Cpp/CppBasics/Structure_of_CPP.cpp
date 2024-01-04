862713037a05f6e6310a6d29a21683e7e474976f
letmesoloall <letmesoloall@gmail.com>
2024-01-04 22:15:53 +0530

    Day0 CPP

---
+++ Cpp/CppBasics/Structure_of_CPP.cpp
@@ -0,0 +1,27 @@
+// Hello world program !
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    std :: cout << "Hello World!";
+    std:: cout << "First CPP program";
+
+    // AFTER USING NAMESPACE STD :
+
+    cout<< "After Namespace";
+
+// instead of we can write like int main() {std ::cout <<"Hello World!;"}
+// for multiple lines we can sperate statements using colon
+
+/*
+This
+is for 
+Mulitline comments
+
+*/
+
+
+
+
+}
---
+++ Cpp/CppBasics/Variables_Datatypes.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // variables : Declaration and Initialization of variables
+    
+    int a; // initialization of variable a of integer datatype
+
+    int b(10); // initialization and declaration of variable b with 10 (integer)
+
+    int c{100}, d{26};
+
+    int e,f;
+
+    cout << e << " " << f;
+
+    cout << c << " " << d << " " << b << " "<< a;
+
+    // primitive datatype :
+
+    /*
+        Integer
+        float
+        Character
+        Boolean
+    */
+
+
+
+}
