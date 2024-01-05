91c75524a6f0dddbacdd27c35c1f89e9bca72333
letmesoloall <letmesoloall@gmail.com>
2024-01-05 06:21:51 +0530

    Day 1 of CPP

---
+++ Cpp/CppBasics/InputOutput.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    // input using cin
+
+    float amount1;
+    float amount2;
+    cout << "Enter Amount 1 you received : ";
+    cin >> amount1;
+
+    cout << endl;
+
+    cout << "Enter Amount 2 you received : ";
+    cin >> amount2;
+
+    cout << endl;
+
+    cout << "Sum of Amount 1 and Amount 2 : " << amount1 + amount2 << endl;
+
+}
--- Cpp/CppBasics/Variables_Datatypes.cpp
+++ Cpp/CppBasics/Variables_Datatypes.cpp
@@ -17,6 +17,19 @@ int main(){
 
     cout << c << " " << d << " " << b << " "<< a;
 
+   // Type deduction : auto (compiler automatcally specifies the datatype)
+
+   auto var (13.56);
+   cout << " " << var << endl;
+
+   // endl keyword (ends the current line)
+
+   cout << "Hello world is in" << endl;
+   cout << "    New Line" << endl;
+
+
+
+
     // primitive datatype :
 
     /*
@@ -24,8 +37,35 @@ int main(){
         float
         Character
         Boolean
+
+
+        // INCLUDES TYPE MODIFIERS :
+
+        signed
+        unsigned
+        long 
+        short
     */
 
+   //integer
+   int IntVar = 56;
+    cout << "Integer variable = " << IntVar << " and size of variable is : "
+        << sizeof(IntVar) << endl;
+
+    // float
+    float FloatVar {234.567};
+    cout << "Float variable = " << FloatVar << " and size of variable is : "
+        << sizeof(FloatVar) << endl;
+
+    // char
+    char CharVar ('T');
+    cout << "Char variable = " << CharVar << " and size of variable is : "
+        << sizeof(CharVar) << endl;
+    
+
+    // concepts of Type modifiers
+
+    return 0;
 
 
 }
